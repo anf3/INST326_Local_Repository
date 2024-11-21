279b36381d6d1cc4232be8e46a81a2c949bc70c8
anf3 <ale12349@terpmail.umd.edu>
2024-11-20 23:17:52 -0500

    Update project3_al.py

--- project3_al.py
+++ project3_al.py
@@ -7,17 +7,17 @@ class Caregivers:
     self.email = email
     self.pay_rate = pay_rate # paid caregivers are paid weekly at $20/hr
     self.hours = 0 # care is required 12 hours per day, 7 days a week (84 hours)
-    self.availability = {}
+    self.schedule = {}
 
-  def schedule_availability(self, availability, shift):
+  def set_schedule(self, shift, availability):
     # each caregiver should have their own availability schedule where they can indicate their availability for each shift
     # availability categories are 'preferred', 'available' (default), and 'unavailable'
     # indicates AM or PM shifts
-    pass
+    self.schedule[shift] = availability
 
-class Schedule:
-  def __init__(self):
-    pass
+  def get_schedule(self, shift, availability):
+    return self.set_schedule
+
 
   
 
